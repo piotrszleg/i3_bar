b79994b20c710bf3cfffffa15f8346bcb0bb5e59
piotrszleg <szlegp@gmail.com>
2020-11-20 17:24:27 +0100

    Switched to classes.

--- bar.py
+++ bar.py
@@ -4,7 +4,6 @@ from i3ipc import Connection, Event
 import threading, queue
 from time import sleep
 
-i3 = Connection()
 q = queue.Queue()
 ui = tk.Tk()
 ui.title("pybar")
@@ -12,61 +11,76 @@ offset_x=0
 offset_y=0
 down=False
 
-def motion(event):
-    if not down:
-        return
-    x=event.x+ui.winfo_x()-offset_x
-    y=event.y+ui.winfo_y()-offset_y
-    ui.geometry(f"+{x}+{y}")
+class Bar(tk.Frame):
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.pack()
+        self.bind('<Motion>', self.motion)
+        self.bind('<ButtonPress>', self.mouse_down)
+        self.bind('<ButtonRelease>', self.mouse_up)
+        self.offset_x=0
+        self.offset_y=0
+        self.down=False
+    
+    def motion(self, event):
+        if not self.down:
+            return
+        x=event.x+ui.winfo_x()-offset_x
+        y=event.y+ui.winfo_y()-offset_y
+        ui.geometry(f"+{x}+{y}")
 
-def mouse_down(event):
-    global down, offset_x, offset_y
-    down=True
-    offset_x=event.x
-    offset_y=event.y
+    def mouse_down(self, event):
+        self.down=True
+        self.offset_x=event.x
+        self.offset_y=event.y
 
-def mouse_up(_):
-    global down
-    down=False
+    def mouse_up(self, _):
+        self.down=False
 
-ui.bind('<Motion>', motion)
-ui.bind('<ButtonPress>', mouse_down)
-ui.bind('<ButtonRelease>', mouse_up)
+    @staticmethod
+    def filter_for_tk(text):
+        return "".join([text[j] for j in range(len(text)) if ord(text[j]) in range(65536)])
 
-def switch_to_workspace(workspace_index):
-    i3.command(f"move container to workspace {workspace_index}")
-    i3.command(f"workspace {workspace_index}")
+    def update_buttons(self, workspaces, callback):
+        for child in self.winfo_children():
+            child.destroy()
+        for workspace in workspaces:
+            if workspace.focused:
+                state="disabled"
+            else:
+                state="normal"
+            command=partial(callback, workspace.num)
+            button = tk.Button(self, pady=2, padx=5, text=self.filter_for_tk(workspace.name or ""), command = command, state=state)
+            button.pack(side="left")
 
-def filter_for_tk(text):
-    return "".join([text[j] for j in range(len(text)) if ord(text[j]) in range(65536)])
+class I3Thread(threading.Thread):
+    def __init__(self, queue):
+        self.queue=queue
+        self.i3 = Connection()
+        super().__init__()
 
-def update_ui(workspaces):
-    for child in ui.winfo_children():
-        child.destroy()
-    for workspace in workspaces:
-        if workspace.focused:
-            state="disabled"
-        else:
-            state="normal"
-        callback=partial(switch_to_workspace, workspace.num)
-        button = tk.Button(ui, pady=2, padx=5, text=filter_for_tk(workspace.name or ""), command = callback, state=state)
-        button.pack(side="left")
+    def i3_update(self):
+        self.queue.put(self.i3.get_workspaces())
+    
+    def run(self):
+        self.i3.on(Event.WORKSPACE_FOCUS, (lambda _, __: self.i3_update()))
+        # TOFIX: actually wait for the app
+        sleep(1)
+        self.i3.command("[title=\"pybar\"] floating enable, sticky enable")
+        self.i3_update()
+        self.i3.main()
+    
+    def switch_to_workspace(self, workspace_index):
+        print(workspace_index)
+        self.i3.command(f"move container to workspace {workspace_index}")
+        self.i3.command(f"workspace {workspace_index}")
 
-def i3_update():
-    q.put(i3.get_workspaces())
-
-def i3_thread():
-    i3.on(Event.WORKSPACE_FOCUS, (lambda _, __: i3_update()))
-    # TOFIX: actually wait for the app
-    sleep(1)
-    i3.command("[title=\"pybar\"] floating enable, sticky enable")
-    i3_update()
-    i3.main()
-
-threading.Thread(target=i3_thread).start()
+i3=I3Thread(q)
+i3.start()
+bar=Bar(ui)
 while True:
     ui.update()
     try:
-        update_ui(q.get_nowait())
+        bar.update_buttons(q.get_nowait(), i3.switch_to_workspace)
     except queue.Empty:
         pass
