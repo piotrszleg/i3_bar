1da6c0f05d48bfad28c9a5db8dffe4035bfd7c88
piotrszleg <szlegp@gmail.com>
2020-12-15 03:40:42 +0100

    Fixed incorrect global references. Experiments with alpha attribute in i3.

--- bar.py
+++ bar.py
@@ -5,19 +5,16 @@ import threading, queue
 from time import sleep
 
 q = queue.Queue()
-ui = tk.Tk()
-ui.title("pybar")
-offset_x=0
-offset_y=0
-down=False
 
-class Bar(tk.Frame):
-    def __init__(self, master=None):
-        super().__init__(master)
-        self.pack()
+class Bar(tk.Tk):
+    def __init__(self):
+        super().__init__()
         self.bind('<Motion>', self.motion)
-        self.bind('<ButtonPress>', self.mouse_down)
-        self.bind('<ButtonRelease>', self.mouse_up)
+        self.bind('<ButtonPress-3>', self.mouse_down)
+        self.bind('<ButtonRelease-3>', self.mouse_up)
+        self.title("pybar")
+        self.wait_visibility(self)
+        self.wm_attributes('-alpha', 0.1)
         self.offset_x=0
         self.offset_y=0
         self.down=False
@@ -25,9 +22,9 @@ class Bar(tk.Frame):
     def motion(self, event):
         if not self.down:
             return
-        x=event.x+ui.winfo_x()-offset_x
-        y=event.y+ui.winfo_y()-offset_y
-        ui.geometry(f"+{x}+{y}")
+        x=event.x+self.winfo_x()-self.offset_x
+        y=event.y+self.winfo_y()-self.offset_y
+        self.geometry(f"+{x}+{y}")
 
     def mouse_down(self, event):
         self.down=True
@@ -50,7 +47,7 @@ class Bar(tk.Frame):
             else:
                 state="normal"
             command=partial(callback, workspace.num)
-            button = tk.Button(self, pady=2, padx=5, text=self.filter_for_tk(workspace.name or ""), command = command, state=state)
+            button = tk.Button(self, pady=1, padx=5, text=self.filter_for_tk(workspace.name or ""), command = command, state=state)
             button.pack(side="left")
 
 class I3Thread(threading.Thread):
@@ -71,15 +68,14 @@ class I3Thread(threading.Thread):
         self.i3.main()
     
     def switch_to_workspace(self, workspace_index):
-        print(workspace_index)
         self.i3.command(f"move container to workspace {workspace_index}")
         self.i3.command(f"workspace {workspace_index}")
 
+bar=Bar()
 i3=I3Thread(q)
 i3.start()
-bar=Bar(ui)
 while True:
-    ui.update()
+    bar.update()
     try:
         bar.update_buttons(q.get_nowait(), i3.switch_to_workspace)
     except queue.Empty:
