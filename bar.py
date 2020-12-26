e4fc1961053d4cfdb067060fe6572322e4c21837
piotrszleg <szlegp@gmail.com>
2020-12-26 19:38:10 +0100

    Tooltips showing opened windows. CSS style moved to style.css.

--- bar.py
+++ bar.py
@@ -15,28 +15,12 @@ q = queue.Queue()
 
 BUTTON_SIZE=30
 
-class Bar():
+class Bar(Gtk.Window):
     def __init__(self):
-        super().__init__()
-        CSS = b"""
-        #toplevel {
-            background-color: rgba(0, 0, 0, 0);
-            min-height:0px;
-            padding:0;
-            margin:0;
-        }
-        button {
-            background-image: none;
-            background-color: rgba(0, 0, 0, 0.7);
-            padding:0px;
-            margin:0px;
-            border:0px;
-            min-height:0px;
-        }
-        """
+        super().__init__(title="pybar", name="toplevel")
 
         style_provider = Gtk.CssProvider()
-        style_provider.load_from_data(CSS)
+        style_provider.load_from_path("style.css")
 
         Gtk.StyleContext.add_provider_for_screen(
             Gdk.Screen.get_default(),
@@ -44,58 +28,49 @@ class Bar():
             Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION
         )
 
-        window = Gtk.Window(title="pybar", name="toplevel")
-        screen = window.get_screen()
+        screen = self.get_screen()
         visual = screen.get_rgba_visual()
-        window.set_visual(visual)
+        self.set_visual(visual)
         self.box=Gtk.Box(spacing=0)
-        window.add(self.box)
-        window.show_all()
-        window.connect("destroy", Gtk.main_quit)
-        self.window=window
+        self.add(self.box)
+        self.show_all()
+        self.connect("destroy", Gtk.main_quit)
         self.screen=screen
     
-    def motion(self, event):
-        if not self.down:
-            return
-        x=event.x+self.winfo_x()-self.offset_x
-        y=event.y+self.winfo_y()-self.offset_y
-        self.geometry(f"+{x}+{y}")
-
-    def mouse_down(self, event):
-        self.down=True
-        self.offset_x=event.x
-        self.offset_y=event.y
-
-    def mouse_up(self, _):
-        self.down=False
-
     def update(self):
         Gtk.main_iteration()
 
     @staticmethod
     def filter_for_tk(text):
-        return "".join([text[j] for j in range(len(text)) if ord(text[j]) in range(65536)])
+        return ("".join([text[j] for j in range(len(text)) if ord(text[j]) in range(65536)]))
 
     def update_buttons(self, workspaces, callback):
         for child in self.box.get_children():
             self.box.remove(child)
+        buttons_count=0
         for workspace in workspaces:
+            if workspace.type!="workspace" or workspace.name=="__i3_scratch":
+                continue
             button1 = Gtk.Button(label=self.filter_for_tk(workspace.name or ""))
             button1.set_margin_bottom(0)
             button1.set_margin_top(0)
             button1.num=workspace.num
             
+            children="\n".join((self.filter_for_tk(child.name or "") for child in workspace.descendants() if child.name))
+            button1.set_tooltip_text(children)
+            
             button1.connect("clicked", lambda button: callback(button.num))
             self.box.pack_start(button1, True, True, 0)
-
-        width=len(workspaces)*BUTTON_SIZE
+            
+            buttons_count+=1
+        
+        width=buttons_count*BUTTON_SIZE
         height=BUTTON_SIZE
         self.box.set_size_request(width, height)
-        self.window.move(self.screen.get_width()-width, 0)
-        self.window.set_size_request(width, height)
-        self.window.set_resizable(False)
-        self.window.show_all()
+        self.move(self.screen.get_width()-width, 0)
+        self.set_size_request(width, height)
+        self.set_resizable(False)
+        self.show_all()
 
 class I3Thread(threading.Thread):
     def __init__(self, queue):
@@ -104,10 +79,12 @@ class I3Thread(threading.Thread):
         super().__init__()
 
     def i3_update(self):
-        self.queue.put(self.i3.get_workspaces())
+        self.queue.put(self.i3.get_tree().descendants())
     
     def run(self):
-        self.i3.on(Event.WORKSPACE_FOCUS, (lambda _, __: self.i3_update()))
+        self.i3.on(Event.WORKSPACE, (lambda _, __: self.i3_update()))
+        self.i3.on(Event.WINDOW_NEW, (lambda _, __: self.i3_update()))
+        self.i3.on(Event.WINDOW_CLOSE, (lambda _, __: self.i3_update()))
         # TOFIX: actually wait for the app
         sleep(1)
         self.i3.command("[title=\"pybar\"] floating enable, sticky enable")
