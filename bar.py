91e1ae87590c6b6f39bd74c1a31d321f0f319aba
piotrszleg <szlegp@gmail.com>
2020-12-28 02:57:59 +0100

    Time related fixes. Replaced sleep(1) with proper event based solution. Displaying clock changing to date view. Removed filter_for_tk static method.

--- bar.py
+++ bar.py
@@ -4,16 +4,17 @@ gi.require_version("Gdk", "3.0")
 gi.require_version("Gtk", "3.0")
 
 from gi.repository import Gdk
-from gi.repository import Gtk
+from gi.repository import Gtk, GObject
 
 from functools import partial
 from i3ipc import Connection, Event
 import threading, queue
-from time import sleep
+import time
 
 q = queue.Queue()
 
 BUTTON_SIZE=30
+TIME_WIDTH=70
 
 class Bar(Gtk.Window):
     def __init__(self):
@@ -22,49 +23,75 @@ class Bar(Gtk.Window):
         style_provider = Gtk.CssProvider()
         style_provider.load_from_path("style.css")
 
+        self.screen = self.get_screen()
+
         Gtk.StyleContext.add_provider_for_screen(
-            Gdk.Screen.get_default(),
+            self.screen,
             style_provider,
             Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION
         )
-
-        screen = self.get_screen()
-        visual = screen.get_rgba_visual()
+        visual = self.screen.get_rgba_visual()
         self.set_visual(visual)
         self.box=Gtk.Box(spacing=0)
         self.add(self.box)
         self.show_all()
         self.connect("destroy", Gtk.main_quit)
-        self.screen=screen
-    
+        self.show_date=False
+        self.time_label=None
+        GObject.timeout_add(1000, self.update_time_label)
+
     def update(self):
         Gtk.main_iteration()
 
-    @staticmethod
-    def filter_for_tk(text):
-        return ("".join([text[j] for j in range(len(text)) if ord(text[j]) in range(65536)]))
+    def new_button(self, name, callback):
+        button = Gtk.Button(label=name)
+        button.set_margin_bottom(0)
+        button.set_margin_top(0)
+        button.connect("clicked", lambda _: callback())
+        self.box.pack_start(button, True, True, 0)
+        return button
 
+    def new_workspace_button(self, name, number, tooltip, callback):
+        button = self.new_button(name, lambda: callback(number))
+        button.set_tooltip_text(tooltip)
+        
+    def create_button_for_workspace(self, workspace, callback):
+        children=(
+            child.name
+            for child in workspace.descendants()
+            if child.name and child.name!="pybar")
+        tooltip="\n".join(children)
+        self.new_workspace_button(workspace.name or "", workspace.num, tooltip, callback)
+
+    def switch_hour_date(self):
+        self.show_date=not self.show_date
+        self.update_time_label()
+
+    def update_time_label(self):
+        if self.time_label!=None:
+            named_tuple = time.localtime() # get struct_time
+            if self.show_date:
+                time_string = time.strftime("%m/%d/%y", named_tuple)
+            else:
+                time_string = time.strftime("%H:%M%p", named_tuple)
+            self.time_label.set_label(time_string)
+            self.show_all()
+        return True
+    
     def update_buttons(self, workspaces, callback):
         for child in self.box.get_children():
             self.box.remove(child)
         buttons_count=0
         for workspace in workspaces:
-            if workspace.type!="workspace" or workspace.name=="__i3_scratch":
-                continue
-            button1 = Gtk.Button(label=self.filter_for_tk(workspace.name or ""))
-            button1.set_margin_bottom(0)
-            button1.set_margin_top(0)
-            button1.num=workspace.num
-            
-            children="\n".join((self.filter_for_tk(child.name or "") for child in workspace.descendants() if child.name))
-            button1.set_tooltip_text(children)
-            
-            button1.connect("clicked", lambda button: callback(button.num))
-            self.box.pack_start(button1, True, True, 0)
-            
-            buttons_count+=1
+            if workspace.type=="workspace" and workspace.name!="__i3_scratch":
+                self.create_button_for_workspace(workspace, callback)
+                buttons_count+=1
         
-        width=buttons_count*BUTTON_SIZE
+        self.time_label=self.new_button("", self.switch_hour_date)
+        self.time_label.set_size_request(TIME_WIDTH, BUTTON_SIZE)
+        self.update_time_label()
+
+        width=buttons_count*BUTTON_SIZE+TIME_WIDTH
         height=BUTTON_SIZE
         self.box.set_size_request(width, height)
         self.move(self.screen.get_width()-width, 0)
@@ -80,24 +107,30 @@ class I3Thread(threading.Thread):
 
     def i3_update(self):
         self.queue.put(self.i3.get_tree().descendants())
-    
+
+    def on_new_window(self, _, event):
+        if event.container.name=="pybar":
+            self.i3.command("[title=\"pybar\"] floating enable, sticky enable")
+            self.i3.off(self.on_new_window)
+            self.i3_update()
+        
     def run(self):
         self.i3.on(Event.WORKSPACE, (lambda _, __: self.i3_update()))
         self.i3.on(Event.WINDOW_NEW, (lambda _, __: self.i3_update()))
         self.i3.on(Event.WINDOW_CLOSE, (lambda _, __: self.i3_update()))
-        # TOFIX: actually wait for the app
-        sleep(1)
-        self.i3.command("[title=\"pybar\"] floating enable, sticky enable")
-        self.i3_update()
+
+        self.i3.on(Event.WINDOW_NEW, self.on_new_window)
+
         self.i3.main()
     
     def switch_to_workspace(self, workspace_index):
         self.i3.command(f"move container to workspace {workspace_index}")
         self.i3.command(f"workspace {workspace_index}")
 
-bar=Bar()
 i3=I3Thread(q)
 i3.start()
+bar=Bar()
+
 while True:
     bar.update()
     try:
